32fd78eb0ea4edaf8086f221d62e94f01ed1a8f6
JohnVBS <joaovitor.save@hotmail.com>
2024-09-19 22:28:47 -0300

    message kkk

--- src/br/edu/unisep/gestaoeventos/util/Funcoes.java
+++ src/br/edu/unisep/gestaoeventos/util/Funcoes.java
@@ -1,4 +1,18 @@
 package br.edu.unisep.gestaoeventos.util;
 
+import br.edu.unisep.gestaoeventos.models.Inscricao;
+import br.edu.unisep.gestaoeventos.models.Palestrante;
+import br.edu.unisep.gestaoeventos.models.Participante;
+import br.edu.unisep.gestaoeventos.models.Sessao;
+import jdk.jfr.Event;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Funcoes {
+    private List<Event> eventos = new ArrayList<>();
+    private List<Participante> participantes = new ArrayList<>();
+    private List<Palestrante> palestrantes = new ArrayList<>();
+    private List<Sessao> sessoes = new ArrayList<>();
+    private List<Inscricao> inscricoes = new ArrayList<>();
 }
